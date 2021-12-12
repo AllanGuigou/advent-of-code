0b768ee7ccaa9680883c9fbe6ed6f2d7bd844718
AllanGuigou <allan.guigou@gmail.com>
2021-12-12 16:47:23 -0500

    Stackoverflow: Day 12 Part 2
    It appears that doing a BFS is causing an overflow, swap to DFS.

--- 2021/day12/Day12.fs
+++ 2021/day12/Day12.fs
@@ -7,9 +7,14 @@ let parseCaveConnection (line: string) =
     let backward = (snd forward, fst forward)
 
     seq {
-        yield forward
+        if snd forward <> "start" && fst forward <> "end" then
+            yield forward
+        else
+            printfn "forward %A" forward
         if snd backward <> "start" && fst backward <> "end" then
             yield backward
+        else
+            printfn "backward %A" backward 
     }
 
 printfn "%A" <| parseCaveConnection("start-A")
@@ -21,15 +26,23 @@ let pathToString (path: List<string>) =
     path
     |> List.reduce (fun accum cave -> sprintf "%s,%s" accum cave)
 
-let shouldVisitCave (path: string list, cave: string) =
-    if cave <> cave.ToUpper() then
-        not (path |> List.contains cave)
-    else
+let pathDoesNotIncludeLowerCaseCaveTwice (path: string list) =
+    path
+    |> List.filter (fun x -> x <> "start" && x <> "end" )
+    |> List.filter (fun x -> x <> x.ToUpper())
+    |> List.groupBy (fun x -> x)
+    |> Map.ofList
+    |> Map.filter (fun key value -> value |> List.length > 1)
+    |> Map.count = 0
+
+
+printfn "%A" (pathDoesNotIncludeLowerCaseCaveTwice ["start"; "HN"; "c"; "b"; "a"])
+
+let shouldVisitCave (path: string list, cave: string, allowLowerCaseCaveTwice: bool) =
+    if cave = cave.ToUpper() || (allowLowerCaseCaveTwice && pathDoesNotIncludeLowerCaseCaveTwice path) then
         true
-
-List.append <| ["bar"]
-
-["bar" ] |> List.append ["foo"]
+    else 
+        not (path |> List.contains cave)
 
 type Day12() =
     inherit Base.Day()
@@ -41,7 +54,8 @@ type Day12() =
             |> Map.ofSeq
             |> Map.map (fun cave connections -> connections |> Seq.map (fun connection -> snd connection) |> Seq.toList)
 
-        // BFS using "queue"
+
+        // BFS using queue
         let mutable cavesToVisit: string list list = [ ["start"] ]
         let mutable paths = 0 
 
@@ -54,10 +68,35 @@ type Day12() =
                 printfn "%s" (pathToString current)
             else
                 for cave in caveSystem.[current |> List.last] do
-                    if shouldVisitCave (current, cave) then
+                    if shouldVisitCave (current, cave, false) then
                         cavesToVisit <- cavesToVisit @ [current @ [cave]]
 
         paths
 
     override this.partTwo(input: seq<string>): int = 
-        failwith "Not Implemented"
+        let foo = input |> Seq.collect parseCaveConnection
+
+        let caveSystem =
+            input
+            |> Seq.collect parseCaveConnection
+            |> Seq.groupBy (fun connection -> fst connection)
+            |> Map.ofSeq
+            |> Map.map (fun cave connections -> connections |> Seq.map (fun connection -> snd connection) |> Seq.toList)
+
+        // BFS using queue
+        let mutable cavesToVisit: string list list = [ ["start"] ]
+        let mutable paths = 0 
+
+        while cavesToVisit |> List.length > 0 do
+            let current = cavesToVisit.Head
+            cavesToVisit <- pop cavesToVisit
+
+            if current |> List.last = "end" then
+                paths <- paths + 1
+                printfn "%s" (pathToString current)
+            else
+                for cave in caveSystem.[current |> List.last] do
+                    if shouldVisitCave (current, cave, true) then
+                        cavesToVisit <- cavesToVisit @ [current @ [cave]]
+
+        paths
