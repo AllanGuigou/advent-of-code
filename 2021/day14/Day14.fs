f673c690fd6d4495faa01df872b9b0905231745f
AllanGuigou <allan.guigou@gmail.com>
2021-12-14 20:35:12 -0500

    Complete Day 14 Part 2

--- 2021/day14/Day14.fs
+++ 2021/day14/Day14.fs
@@ -4,13 +4,13 @@ let decrement (letters: Map<string, uint64>, key: string) =
     letters.Add(key, letters.[key] - uint64 (1))
 
 
-let increment (letters: Map<string, uint64>, key: string) =
+let increment (letters: Map<string, uint64>, key: string, count: uint64) =
     letters.Add(
         key,
         if letters.ContainsKey(key) then
-            letters.[key] + uint64 (1)
+            letters.[key] + count
         else
-            uint64 (1)
+            count
     )
 
 type Day14() =
@@ -38,7 +38,7 @@ type Day14() =
         let mutable template = templates.Head
 
         for char in template do
-            letters <- increment (letters, char.ToString())
+            letters <- increment (letters, char.ToString(), uint64(1))
 
         for step = 0 to steps - 1 do
             let mutable next = System.Text.StringBuilder()
@@ -47,7 +47,7 @@ type Day14() =
                 let polymerPair = template.Substring(i, 2)
                 let value = insertionMap.[polymerPair]
 
-                letters <- increment (letters, value)
+                letters <- increment (letters, value, uint64(1))
 
                 next <- next.Append(polymerPair.Substring(0, 1).Insert(1, value))
 
@@ -84,39 +84,35 @@ type Day14() =
 
 
         let mutable polymerPairs = Map.empty<string, uint64>
+        let mutable polymerCounts = Map.empty<string, uint64>
+
+        for i = 0 to templates.Head.Length - 1 do
+            polymerCounts <- increment (polymerCounts, templates.Head.[i].ToString(), uint64(1))
 
         for i = 0 to templates.Head.Length - 2 do
             let polymerPair = templates.Head.Substring(i, 2)
-            polymerPairs <- increment (polymerPairs, polymerPair)
+            polymerPairs <- increment (polymerPairs, polymerPair, uint64(1))
 
-        let steps = 1
-        printfn "%A" polymerPairs
+        let steps = 40
 
         for i = 0 to steps - 1 do
             let mutable tmp = Map.empty<string, uint64>
 
             for pair in polymerPairs.Keys do
                 let newPairs = foo (pair)
+                let count = polymerPairs.[pair]
+                polymerCounts <- increment (polymerCounts, insertionMap.[pair].ToString(), count)
 
                 for np in newPairs do
-                    tmp <- increment (tmp, np)
+                    tmp <- increment (tmp, np, count)
 
             printfn "%A" tmp
             polymerPairs <- tmp
 
-        let result =
-            (polymerPairs
-             |> Map.toSeq
-             |> Seq.collect (fun kvp ->
-                 seq {
-                     ((fst kvp).[0], snd kvp)
-                     ((fst kvp).[1], snd kvp)
-                 })
-             |> Seq.groupBy (fun x -> fst x)
-             |> Seq.map (fun x -> (fst x, snd x |> Seq.sumBy (fun y -> snd y))))
+        for kvp in polymerCounts |> Map.toSeq do
+            printfn "%s -> %u" (fst kvp) (snd kvp)
 
-        for kvp in result do
-            printfn "%c -> %u" (fst kvp) (snd kvp)
+        printfn "Big Answer %u" (polymerCounts |> Map.values |> Seq.max |> (-) <| (polymerCounts |> Map.values |> Seq.min))
 
 
         0
