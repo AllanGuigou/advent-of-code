c1942b27fc6a7c085f392a65b74adaa7aecde570
Allan Guigou <allan.guigou@gmail.com>
2021-12-16 09:39:55 -0500

    Complete Day 15 Part 2

--- 2021/day15/Day15.fs
+++ 2021/day15/Day15.fs
@@ -1,6 +1,10 @@
 module Day15
 
+open FSharpx.Collections
+open FSharpx.Collections
+
 let max = 2147483647
+
 
 let print (rows: int, columns: int) (risks: Map<int * int, int>) =
     for row = 0 to rows - 1 do
@@ -39,8 +43,7 @@ let repeatIndex (repeat: int, rows: int, columns: int) (indexRisk: (int * int) *
             for cc = 0 to repeat - 1 do
                 let risk = (risk + cc + rr)
 
-                let scaledRisk =
-                    (if risk > 9 then risk % 10 + 1 else risk)
+                let scaledRisk = (risk - 1) % 9 + 1
 
                 printf "%d" scaledRisk
 
@@ -49,6 +52,8 @@ let repeatIndex (repeat: int, rows: int, columns: int) (indexRisk: (int * int) *
             printfn ""
     }
     |> Seq.toArray
+
+repeatIndex (5, 15, 15) ((0, 0), 2)
 
 let parseRisks (input: seq<string>, repeat: int) =
     let rows = input |> Seq.length
@@ -75,14 +80,48 @@ let bellmanFord (risks: Map<int * int, int>) =
         |> Map.add (0, 0) 0
 
     for index in risks.Keys do
-        let adjacent = adjacentIndexRisk (index, risks)
+        let adjacent = adjacentIndexRisk (index, dist)
 
         for a in adjacent do
 
             if dist.[index] + snd a < dist.[fst a] then
-                dist <- dist |> Map.add (fst a) (dist.[index] + snd a)
+                dist <-
+                    dist
+                    |> Map.add (fst a) (dist.[index] + risks.[fst a])
 
     dist
+
+
+let djikstra (risks: Map<int * int, int>, rows: int, columns: int) =
+    let mutable bestAt = Map.empty<(int * int), int>
+    let mutable pq = Heap.empty false
+
+    pq <- pq |> Heap.insert (0, (0, 0))
+    bestAt <- bestAt |> Map.add (0, 0) 0
+
+    while pq.Length > 0 do
+        let (head, tail) = pq |> Heap.uncons
+        pq <- tail
+        let (cost, coord) = head
+
+        // iterate over all adjacent index's updating their bestAt cost if it is lower
+        for air in adjacentIndexRisk (coord, risks) do
+            let (ai, r) = air
+
+            let aiCostOrMax =
+                if bestAt |> Map.containsKey ai then
+                    cost
+                else
+                    max
+
+            // if there is a shorter path to ai from coord
+            if aiCostOrMax > cost + r then
+                bestAt <- bestAt |> Map.add ai (cost + r)
+                pq <- pq |> Heap.insert (cost + r, ai)
+
+
+    bestAt.[(rows - 1, columns - 1)]
+
 
 type Day15() =
     inherit Base.Day()
@@ -97,6 +136,7 @@ type Day15() =
     override this.partTwo(input: seq<string>) : int =
         let (risks, rows, columns) = parseRisks (input, 5)
 
-        let dist = bellmanFord (risks)
-
-        dist.[(rows - 1, columns - 1)]
+        // my bellmanFord algorithm gave me the incorrect answer, off by 3
+        // let dist = bellmanFord (risks)
+        // dist.[(rows - 1, columns - 1)]
+        djikstra (risks, rows, columns)
